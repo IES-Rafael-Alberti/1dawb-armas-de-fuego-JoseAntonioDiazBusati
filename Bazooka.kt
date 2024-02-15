489e1af55a4f985cccaa461ed23fb031b8a54b97
jdiabus511 <jadiaz@iesrafaelalberti.es>
2024-02-15 11:45:59 +0100

    Principales de bazooka y rifle

--- Bazooka.kt
+++ Bazooka.kt
@@ -1,2 +1,11 @@
-class Bazooka {
+class Bazooka(
+    municion:Int,
+    tipoDeMunicion:String,
+    override val danio: Int,
+    override val radio: Radio
+    ):ArmaDeFuego("Bazooka", municion,3,tipoDeMunicion) {
+    init {
+        require(radio == Radio.Intermedio|| radio == Radio.Amplio || radio == Radio.Enorme){"Error radio"}
+
+    }
 }
--- Rifle.kt
+++ Rifle.kt
@@ -1,2 +1,11 @@
-class Rifle {
+class Rifle(
+    municion:Int,
+    tipoDeMunicion:String,
+    override val danio: Int,
+    override val radio: Radio
+    ):ArmaDeFuego("Rifle", municion,2,tipoDeMunicion) {
+    init {
+        require(radio == Radio.Corto || radio == Radio.Intermedio){"Error radio"}
+
+    }
 }
